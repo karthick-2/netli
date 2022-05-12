af82d616ef6829db0cf1e795e7d1aceb14307d16
karthick-2 <60372370+karthick-2@users.noreply.github.com>
2022-05-12 21:52:54 +0530

    Add files via upload

---
+++ ap.vue
@@ -0,0 +1,334 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>kartpoertli</title>
+    <link rel="stylesheet" href="styles.css">
+    <!-- png -->
+
+    
+</head>
+<body>
+
+
+
+
+    <span class="Omega">
+        Omega
+      </span>
+    <img src="img/oval.png"
+    srcset="img/oval@2x.png 2x,
+    img/oval@3x.png 3x"
+    class="Oval">
+    
+        <span class="Build-a-perfect-land">
+            Build a perfect landing page fast.
+          </span>
+
+<!-- svg -->
+
+<img src="img/oval.svg"
+
+class="Oval">
+<span class="You-have-successfull">
+    You have successfully logged in
+  </span>
+  <div class="Design"></div>
+  <!-- jpg -->
+
+<img src="img/logo-2.jpg"
+srcset="img/logo-2@2x.jpg 2x,
+        img/logo-2@3x.jpg 3x"
+class="logo-2">
+
+<!-- png -->
+
+<!-- jpg -->
+
+<div class="Oval"></div>
+<div class="Rectangle"></div>
+
+<!-- png -->
+
+<img src="img/i-phone-body.png"
+     srcset="img/i-phone-body@2x.png 2x,
+             img/i-phone-body@3x.png 3x"
+     class="iPhone-Body">
+     <!-- png -->
+
+<img src="img/path.png"
+srcset="img/path@2x.png 2x,
+        img/path@3x.png 3x"
+class="Path">
+
+<!-- svg -->
+
+<img src="img/path.svg"
+class="Path">
+<!-- jpg -->
+
+
+<!-- png -->
+
+<img src="img/logo-1.jpg"
+     srcset="img/logo-1@2x.jpg 2x,
+             img/logo-1@3x.jpg 3x"
+     class="logo-1">
+     <div class="Landing-1"></div>
+     <!-- jpg -->
+
+
+<!-- png -->
+
+<img src="img/logo-3.png"
+srcset="img/logo-3@2x.png 2x,
+        img/logo-3@3x.png 3x"
+class="logo-3">
+<!-- svg -->
+
+<!-- svg -->
+
+<img src="img/layout-11.svg"
+     class="layout-11">
+
+<!-- png -->
+
+<img src="img/layout-11.png"
+     srcset="img/layout-11@2x.png 2x,
+             img/layout-11@3x.png 3x"
+     class="layout-11">
+     <span class="Multiple-Layouts">
+        Multiple Layouts
+      </span>
+      <span class="With-lots-of-unique">
+        With lots of unique blocks, you can easily build a page without coding.
+      </span>
+      <div class="BG"></div>
+      <!-- png -->
+
+<img src="img/sidebar.png"
+srcset="img/sidebar@2x.png 2x,
+        img/sidebar@3x.png 3x"
+class="sidebar">
+
+<!-- svg -->
+
+<img src="img/sidebar.svg"
+class="sidebar">
+<div class="Oval"></div>
+<div class="Oval"></div>
+<span class="Fully-Responsive">
+    Fully Responsive
+  </span>
+  <span class="With-lots-of-unique">
+    With lots of unique blocks, you can easily build a page without coding.
+  </span>
+  
+  <span class="Create-custom-landin">
+    Create custom landing pages with Omega that converts more visitors than any website. With lots of unique blocks, you can easily build a page without coding.
+  </span>
+  <div class="BG"></div>
+  <span class="Save-tons-of-time-on">
+    Save tons of time on design & coding.
+  </span>
+  <!-- png -->
+
+<img src="img/img.png"
+srcset="img/img@2x.png 2x,
+        img/img@3x.png 3x"
+class="Img">
+
+<!-- jpg -->
+<span class="Next-meeting-on-30-m">
+    Next meeting on 30 mins
+  </span>
+
+  <span class="Project-Discussion-w">
+    Project Discussion with John
+  </span>
+  <div class="Path"></div>
+  <div class="Oval"></div>
+
+<img src="img/bg.svg"
+     class="BG">
+
+<!-- png -->
+
+<img src="img/bg.png"
+     srcset="img/bg@2x.png 2x,
+             img/bg@3x.png 3x"
+     class="BG">
+<!-- jpg -->
+
+<!-- png -->
+
+
+<!-- jpg -->
+
+<!-- png -->
+
+<img src="img/logo-5.png"
+     srcset="img/logo-5@2x.png 2x,
+             img/logo-5@3x.png 3x"
+     class="logo-5">
+
+<img src="img/logo-4.png"
+     srcset="img/logo-4@2x.png 2x,
+             img/logo-4@3x.png 3x"
+     class="logo-4">
+
+<span class="Appointment-with-the">
+        Appointment with the doctor was successful. Move to next!
+      </span>
+<div class="Landing-1"></div>   
+<span class="Omega">Omega </span>
+
+  <div class="Landing-1"></div>
+  <span class="Redesigned-how-you">
+    Redesigned, 
+  how you build landing page!
+  </span>
+
+  <span class="Demos-Pages-Support">
+
+
+
+
+
+        <span class="text-style-1">Demos</span>Pages          Support
+      
+      </span>
+      <div class="BG"></div>
+      <div class="Rectangle"></div>
+      <span class="SaaS-Landing-Startup">
+
+        SaaS Landing<span class="text-style-1">Startup Landing</span>App Landing
+      
+      Desktop Landing
+      
+      </span>
+      <div class="Rectangle"></div>
+      <span class="Youve-got-a-new-pro">
+        You’ve got a new project from David!
+      </span>
+      <span class="Its-everything-you">
+        It’s everything you’ll ever need.
+      </span>
+      <span class="Create-custom-landin">
+        Create custom landing pages with Omega that converts more visitors than any website. With lots of unique blocks, you can easily build a page without coding.
+      </span>
+      <div class="Rectangle-3"></div>
+      <div class="BG"></div>
+      <div class="BG"></div>
+      <span class="Mobile-First-Design">
+        Mobile First Design<span class="text-style-1">on Every Devices</span>
+      </span>
+      <!-- svg -->
+
+<img src="img/icon.svg"
+class="Icon">
+
+<!-- png -->
+
+<img src="img/icon.png"
+srcset="img/icon@2x.png 2x,
+        img/icon@3x.png 3x"
+class="Icon">
+<div class="Rectangle-3"></div>
+<span class="Customers-are-loving">
+    Customers are loving our Omega
+  </span>   
+  <!-- svg -->
+
+<img src="img/bg.svg"
+class="BG">
+
+<!-- png -->
+
+<img src="img/bg (copy 2).png).png"
+srcset="img/bg@2x (copy 2).png 2x,
+        img/bg@3x (copy 2).png 3x"
+class="BG">
+
+<img src="img/bg (copy 1).png).png"
+srcset="img/bg@2x (copy 1).png 2x,
+        img/bg@3x (copy 1).png 3x"
+class="BG">
+<div class="white-curve-3"></div>
+<span class="Get-started">
+    Get started
+  </span>
+  <span class="Create-custom-landin">
+    Create custom landing pages with Omega that converts more visitors than any website. With lots of unique blocks, you can easily build a page without coding.
+  </span>
+  <div class="Rectangle"></div>
+  <span class="Your-name">
+    Your name
+  </span>
+  <div class="Rectangle"></div>
+  <span class="Email-address">
+    Email address
+  </span>
+  <div class="BG"></div>
+  <span class="Get-Started">
+    Get Started
+  </span>
+  <span class="Omega">
+    Omega
+  </span>
+  <span class="About">
+    About
+  </span>
+  <span class="Learn">
+    Learn
+  </span>
+  <span class="Stories">
+    Stories
+  </span>
+  <span class="Hire">
+    Hire
+  </span>
+  <span class="Our-mission-Our-stor">
+    Our mission
+  Our story
+  Team Members
+  </span>
+  <span class="Tutorials-How-it-wor">
+    Tutorials
+  How  it works
+  F.A.Q
+  </span>
+  <span class="Blog-Tech-stories">
+    Blog
+  Tech stories
+  </span>
+  <span class="Career-Freelancers-T">
+    Career
+  Freelancers
+  Trainee
+  </span>
+
+  <span class="-\32 020-UXTheme-All">
+    © 2020 UXTheme, All Rights Reserved
+  </span>
+
+      <span class="Create-custom-landin">
+        Create custom landing pages with Omega that convert more visitors than any website. With lots of unique blocks, you can easily build a page without coding.
+      </span>
+      <span class="Get-Started">
+        Get Started
+      </span>
+      <div class="BG"></div>
+      <span class="Watch-the-intro-vide">
+        Watch the intro video
+      </span>
+      <span class="Popular-teams-who-us">
+        Popular teams who use Omega for appoinments
+      </span>
+      <div class="Line"></div>
+
+      
+</body>
+</html>
